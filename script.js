cfe8eac2dee3a6690fcf95faab8efbce0aa7fd22
SanderVanOff <7okay@mail.ru>
2021-11-30 21:19:51 +0500

    добавлено решение четвертого дз

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-let num = 266219;
+const getString = function(arg){
+  if(typeof arg === "string"){
+    arg.trim();
+    return arg.length > 30 ? arg.slice(0, 30) + '...' : arg;
+  } else {
+    alert(`${arg} - 'это не строка`);
+  }
+};
 
-num = String(num).split('');
-let result = num[0];
-
-for (let i = 1; i < num.length; i++) {
-    result = result * num[i];
-}
-console.log(result);
-
-result = result ** 3;
-
-console.log(String(result).slice(0, 2));
+console.log(getString('Объявить функцию '));
