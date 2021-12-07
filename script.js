2a5262bcaf7a601d5993c6453ce44143fd9b5a8d
SanderVanOff <7okay@mail.ru>
2021-12-07 22:43:58 +0500

    добавление решения д/з 9

--- script.js
+++ script.js
@@ -1,13 +1,98 @@
-let num = 266219;
+const date1 = document.querySelector('.date');
+const date2 = document.querySelector('.date2');
 
-num = String(num).split('');
-let result = num[0];
+const getHoursText = function (num) {
+  switch (true) {
+    case num === 1:
+    case num === 21:
+      return `${num} час`;
 
-for (let i = 1; i < num.length; i++) {
-    result = result * num[i];
-}
-console.log(result);
+    case num > 1 && num < 5:
+    case num > 21 && num < 25:
+      return `${num} часа`;
 
-result = result ** 3;
+    case num > 5 && num < 21:
+      return `${num} часов`;
 
-console.log(String(result).slice(0, 2));
+    default:
+      break;
+  }
+};
+
+
+const getMinutesText = function (num) {
+  switch (true) {
+    case num === 1:
+    case num === 21:
+    case num === 31:
+    case num === 41:
+    case num === 51:  
+      return `${num} минута`;
+
+    case num > 1 && num < 5:
+    case num > 21 && num < 25:
+    case num > 31 && num < 35:
+    case num > 41 && num < 45:
+    case num > 51 && num < 55:  
+      return `${num} минуты`;
+
+    case num === 0:
+    case num >= 5 && num < 21:
+    case num > 24 && num < 31:
+    case num > 34 && num < 41:
+    case num > 44 && num < 51:
+    case num > 54:  
+      return `${num} минут`;
+    default:
+      break;
+  }
+};
+
+const getSecondsText = function(num){
+  switch(true){
+      case num === 1:
+        return `0${num} секунда`;
+      case num === 21:
+      case num === 31:
+      case num === 41:
+      case num === 51:  
+        return `${num} секунда`;
+  
+      case num > 1 && num < 5:
+        return `0${num} секунды`;
+
+      case num > 21 && num < 25:
+      case num > 31 && num < 35:
+      case num > 41 && num < 45:
+      case num > 51 && num < 55:  
+        return `${num} секунды`;
+  
+      case num >= 5 && num < 10:
+        return `0${num} секунд`;
+
+      case num > 9 && num < 21:
+      case num > 24 && num < 31:
+      case num > 34 && num < 41:
+      case num > 44 && num < 51:
+      case num > 54:  
+        return `${num} секунд`;
+
+      case num === 0:
+        return `0${num} секунд`;
+      default:
+        break;
+  }
+};
+
+const getData = function () {
+  date1.textContent = `сегодня ${new Date().toLocaleDateString('ru-RU', {
+    weekday: 'long'
+  })}, ${new Date().toLocaleDateString('ru-RU', {
+    day: 'numeric',
+    month: 'long'
+  })} ${new Date().getFullYear()} года, ${getHoursText(new Date().getHours())} ${getMinutesText(new Date().getMinutes())} ${getSecondsText(new Date().getSeconds())}`;
+  date2.textContent = `${new Date().toLocaleDateString()} - ${new Date().toLocaleTimeString()}`;
+};
+
+
+setInterval(getData, 1000);
